fb769c1a23a70b246fe47bc52277fadeb78fc8c1
Olivier Rasquin <rasquin.olivier@gmail.com>
2020-12-21 18:05:39 +0100

    finish over effects

--- app/assets/scripts/modules/OverlayMenu.js
+++ app/assets/scripts/modules/OverlayMenu.js
@@ -2,20 +2,31 @@ const OverlayMenu = () => {
   const burgerIcon = document.querySelector(".header__burger-icon");
   const closeBtn = document.querySelector(".overlay-menu__close-icon");
   const menu = document.querySelector(".overlay-menu");
+  let menuIsOpen = false;
 
   function openMenu() {
     document.body.classList.add("menu--is-open");
     menu.classList.add("overlay-menu--is-open");
-    console.log(menu);
+    menuIsOpen = true;
   }
 
   function closeMenu() {
     document.body.classList.remove("menu--is-open");
     menu.classList.remove("overlay-menu--is-open");
+    menuIsOpen = false;
   }
 
+  // open menu
   burgerIcon.addEventListener("click", openMenu);
+
+  // close menu (click + esc)
   closeBtn.addEventListener("click", closeMenu);
+
+  window.addEventListener("keydown", (e) => {
+    if (menuIsOpen && e.key === "Escape") {
+      closeMenu();
+    }
+  });
 };
 
 export default OverlayMenu;
